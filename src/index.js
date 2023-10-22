6031ff664f7b3875f864aab1cff187e592cb68f0
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-10-22 19:17:52 +0300

    add: products section

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import './index.scss';
 import header from './modules/header';
 import tabs from './modules/tabs';
 import carousel from './modules/carousel';
+import products from './modules/products';
 
 window.addEventListener('DOMContentLoaded', () => {
  header();
  tabs();
  carousel();
+ products();
 })
---
+++ src/modules/products.js
@@ -0,0 +1,18 @@
+const products = () => {
+/* const wrapper = document.querySelector('.products__wrapper');
+const viewBlock = document.querySelector('.products__view-block'); */
+
+/* wrapper.addEventListener('mouseover', (event) => {
+   if (event.target.classList.contains('products__card')) {
+     viewBlock.classList.add('products__view-block_active');
+   }
+})
+
+wrapper.addEventListener('mouseout', (event) => {
+  if (!event.target.classList.contains('products__card')) {
+    viewBlock.classList.remove('products__view-block_active');
+  }
+}) */
+}
+
+export default products;
