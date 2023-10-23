0c17467959b5e745c56694f0d93a5cafc5be0788
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-10-23 23:21:48 +0300

    finish

--- src/modules/header.js
+++ src/modules/header.js
@@ -4,15 +4,24 @@ const button = document.querySelectorAll('.side-menu__button');
 const burger = document.querySelector('.header__burger');
 const sideMenu = document.querySelector('.side-menu');
 const close = document.querySelector('.side-menu__close');
+const overlay = document.querySelector('.overlay');
 
 close.addEventListener('click', () => {
   sideMenu.classList.remove('side-menu_active');
-  burger.classList.remove('header__burger_burger-active');
+  burger.classList.remove('header__burger_active');
+  overlay.classList.remove('overlay_active');
 });
 
+overlay.addEventListener('click', () => {
+  sideMenu.classList.remove('side-menu_active');
+  burger.classList.remove('header__burger_active');
+  overlay.classList.remove('overlay_active');
+})
+
 burger.addEventListener('click', () => {
- burger.classList.toggle('header__burger_burger-active');
- sideMenu.classList.toggle('side-menu_active')
+ burger.classList.toggle('header__burger_active');
+ sideMenu.classList.toggle('side-menu_active');
+ overlay.classList.toggle('overlay_active');
 })
 
 button.forEach((item, i)=> {
--- src/modules/products.js
+++ src/modules/products.js
@@ -1,18 +1,24 @@
 const products = () => {
-/* const wrapper = document.querySelector('.products__wrapper');
-const viewBlock = document.querySelector('.products__view-block'); */
+const wrapper = document.querySelector('.products__wrapper');
+const viewBlock = document.querySelectorAll('.products__view-block');
+const shareBlock = document.querySelectorAll('.products__like-share');
+const cards = document.querySelectorAll('.products__card');
 
-/* wrapper.addEventListener('mouseover', (event) => {
-   if (event.target.classList.contains('products__card')) {
-     viewBlock.classList.add('products__view-block_active');
-   }
-})
+cards.forEach((card, i) => {
+  card.addEventListener('mouseenter', (event) => {
+    if (event.target.getAttribute('data-id') === String(i)) {
+      viewBlock[i]?.classList.add('products__view-block_active');
+      shareBlock[i]?.classList.add('products__like-share_active');
+    }
+  });
 
-wrapper.addEventListener('mouseout', (event) => {
-  if (!event.target.classList.contains('products__card')) {
-    viewBlock.classList.remove('products__view-block_active');
-  }
-}) */
+  card.addEventListener('mouseleave', (event) => {
+    if (event.target.getAttribute('data-id') === String(i)) {
+      viewBlock[i]?.classList.remove('products__view-block_active');
+      shareBlock[i]?.classList.remove('products__like-share_active');
+    }
+  });
+});
 }
 
 export default products;
